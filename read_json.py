5f6b597119ffeae2643324bf733a74f73c421b8e
luizhcb0 <luizhcb0@gmail.com>
2018-11-23 07:47:57 -0200

    mais coisas

--- read_json.py
+++ read_json.py
@@ -4,4 +4,18 @@ import json
 path = "../pydata-book/datasets/bitly_usagov/example.txt"
 records = [json.loads(line) for line in open(path)]
 
-print(records[0])
+#print(records[0])
+timezones = [rec['tz'] for rec in records if 'tz' in rec and rec['tz'] != '']
+timezones.__len__()
+len(timezones)
+
+def get_counts(seq):
+    counts = {}
+    for x in seq:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+    return counts
+
+c = get_counts(timezones)
